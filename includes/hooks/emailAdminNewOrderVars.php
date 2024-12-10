ceb8ec03437b45aa77e87f5275d79ac6223832ec
Jordan Schelew <jordan@websavers.ca>
2024-12-10 11:17:27 -0400

    Simplify logging

--- includes/hooks/emailAdminNewOrderVars.php
+++ includes/hooks/emailAdminNewOrderVars.php
@@ -1,6 +1,6 @@
 <?php
 
-// Version 1.0
+// Version 1.1
 // Currently adds:
 // - Whether this is the client's first order or not
 
@@ -15,7 +15,13 @@ define('BODY_REGEX', '/(Service|Addon) ID # (\d+) was just auto provisioned/');
 // https://developers.whmcs.com/hooks-reference/everything-else/#emailpresend
 add_hook('EmailPreSend', 1, function($vars) {
 
-    //logActivity('[Hook emailAdminNewOrderVars] EmailPreSend Triggered with vars: ' . print_r($vars));
+    if( ! function_exists('eanoLog') ){
+        function eanoLog($message){
+            logActivity("[Hook emailAdminNewOrderVars] $message");
+        }
+    }
+
+    //hookLog('EmailPreSend Triggered with vars: ' . print_r($vars));
 
     if ($vars['messagename'] != 'WHMCS New Order Notification') return; //only proceed if it's the new order notification
 
@@ -25,10 +31,16 @@ add_hook('EmailPreSend', 1, function($vars) {
 
     // https://developers.whmcs.com/api-reference/getorders/
     $results = localAPI('GetOrders', array('userid' => $clientid));
+    if ( $results['result'] != 'success' ){
+        eanoLog('GetOrders API Call failure.');
+        return '';
+    }
+
+    $orders = $results['numreturned'];
     	
     $merge_fields = [];
     if (!array_key_exists('client_firstorder', $vars['mergefields'])) {
-        $merge_fields['client_firstorder'] = ($results['numreturned'] == 1)? 'Yes':'No';
+        $merge_fields['client_firstorder'] = ($orders === 1)? 'Yes':'No';
     }
     return $merge_fields;
 
@@ -36,7 +48,12 @@ add_hook('EmailPreSend', 1, function($vars) {
 
 //Output additional merge fields in the list when editing an email template
 add_hook('EmailTplMergeFields', 1, function($vars) {
+
+    if ( $vars['type'] != 'admin' ) return '';
+
     $merge_fields = [];
     $merge_fields['client_firstorder'] = "New Client?";
+
     return $merge_fields;
+
 });
